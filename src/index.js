87ad94ba15e2d59a9e4b9dd2ef0fb3a00f65df33
Mwila <bupemwila@icloud.com>
2026-08-29 21:37:00 +0200

    dfkjnkdf

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './assets/css/style.min.css';
+import {g} from '../../utils/dom-utils.js'
+
+import diaspora from './assets/img/diaspora.png'
+import feebs from './assets/img/feebs.png'
+import kalulu from './assets/img/kalulu.png'
+import kaluluAndroid from './assets/img/kaluluAndroid.png'
+import kudu from './assets/img/kudu.png'
+import profile from './assets/img/pp.jpg'
+
+g("diaspora").src = diaspora
+g("feebs").src = feebs
+g("kalulu").src = kalulu
+g("kaluluAndroid").src = kaluluAndroid
+g("kudu").src = kudu
+
+
+g("profile").src = profile
+
+
+
 import './assets/js/scripts';
