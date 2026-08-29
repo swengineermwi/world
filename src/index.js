3849f320f899375873ef6091912668fc38a6be2a
Mwila <bupemwila@icloud.com>
2026-08-29 21:46:17 +0200

    kjsnkjdf

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import './assets/css/style.min.css';
-import {g} from '../../utils/dom-utils.js'
+import { g } from '../../utils/dom-utils.js';
 
-import diaspora from './assets/img/diaspora.png'
-import feebs from './assets/img/feebs.png'
-import kalulu from './assets/img/kalulu.png'
-import kaluluAndroid from './assets/img/kaluluAndroid.png'
-import kudu from './assets/img/kudu.png'
-import profile from './assets/img/pp.jpg'
-
-g("diaspora").src = diaspora
-g("feebs").src = feebs
-g("kalulu").src = kalulu
-g("kaluluAndroid").src = kaluluAndroid
-g("kudu").src = kudu
-
-
-g("profile").src = profile
-
-
+import diaspora from './assets/img/diaspora.png';
+import feebs from './assets/img/feebs.png';
+import kalulu from './assets/img/kalulu.png';
+import kaluluAndroid from './assets/img/kaluluAndroid.png';
+import kudu from './assets/img/kudu.png';
+import profile from './assets/img/pp.jpg';
 
 import './assets/js/scripts';
+
+g('diaspora').src = diaspora;
+g('feebs').src = feebs;
+g('kalulu').src = kalulu;
+g('kaluluAndroid').src = kaluluAndroid;
+g('kudu').src = kudu;
+
+g('profile').src = profile;
